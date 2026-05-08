52269935fa19f8d9a2bc59f60d7120ca5f89b67d
Zolender <ndeingare@gmail.com>
2026-05-08 10:53:53 +0200

    feat: add db.json test data and implement GET endpoint in server.ts to serve it

--- server.ts
+++ server.ts
@@ -1,10 +1,23 @@
 import express from "express";
-
+import fs from "fs"
+import { Response, Request } from "express";
 
 const app = express()
 
 app.use(express.json())
 
+app.get("/", async (req: Request, res: Response)=>{
+    fs.readFile("./db.json", "utf8", (err, data)=>{
+        if(err){
+            console.error(err.message)
+            res.status(500).json({message: "Something went wrong on the server side", error: err.message})
+            return
+        }
+        const db = JSON.parse(data)
+        console.log("Data displaying")
+        res.status(200).json(db)
+    })
+})
 
 
 
