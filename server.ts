f80c9004a197245424d0f1d6b0ca2d7f11b7504f
Zolender <ndeingare@gmail.com>
2026-05-08 11:20:33 +0200

    feat: implement POST endpoint to create items and add Item type definition

--- server.ts
+++ server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import fs from "fs"
 import { Response, Request } from "express";
+import { Item } from "./types/item";
+import { randomUUID } from "crypto";
+
 
 const app = express()
-
 app.use(express.json())
 
 app.get("/", async (req: Request, res: Response)=>{
@@ -19,6 +21,25 @@ app.get("/", async (req: Request, res: Response)=>{
     })
 })
 
+app.post("/create", (req: Request, res: Response)=>{
+    const data = {...req.body, id: randomUUID()}
+    if(!isValidItem(data)){
+        res.status(400).json({message: "Bad request, try again later"})
+    }else{
+        fs.writeFile("./db.json", JSON.stringify(data), (err)=>{
+            if(err){
+                console.error(err.message)
+                res.status(500).json({message: "Something went wrong on the server side. Try sth else in the meantime"})
+                return
+            }
+        })
+        res.status(201).json({message: "post created successfully!"})
+    }
+})
+
+function isValidItem(obj: Object): boolean{
+    return ( obj.hasOwnProperty("id") && obj.hasOwnProperty("name") && obj.hasOwnProperty("description") && obj.hasOwnProperty("price"))
+}
 
 
 app.listen(3000, ()=>{
